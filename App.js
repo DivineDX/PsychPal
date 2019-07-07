7fdafe29280fd142d86e90eaeb8edf65732d43a2
alxkohh <49210156+alxkohh@users.noreply.github.com>
2019-07-07 21:41:12 +0800

    Planner without "CALL" button to go into video chat
    Made use of the card template from Native Base. Styling and functionality (onClick) not applied, just a raw front end skeleton.

---
+++ App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import Planner from "./Planner"
+
+function App() {
+  return <Planner/>
+}
+
+export default App
