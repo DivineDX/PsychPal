9ce5cd24cb8beaa62ff4efcc1cfa8630e51d0be9
alxkohh <49210156+alxkohh@users.noreply.github.com>
2019-07-23 23:18:03 +0800

    refactor search.js

--- frontendapp/src/Containers/PatientUI/Search.js
+++ frontendapp/src/Containers/PatientUI/Search.js
@@ -1,5 +1,5 @@
 import { View, ScrollView } from 'react-native'
-import { Button, SearchBar } from 'react-native-elements'
+import { Button, SearchBar, ListItem } from 'react-native-elements'
 import React from 'react';
 import DoctorInfoCard from '../../Components/Cards/DoctorInfoCard'
   
@@ -22,31 +22,108 @@ const doctors = [
 
    {
     "name": "David Luiz",
-    "language1": "Portugese",
+    "language1": 'Spanis',
     "language2": null,
     "language3": null,
-    "profile_picture_uri": "https://hu.wikipedia.org/wiki/David_Luiz#/media/F%C3%A1jl:David_Luiz_and_Ahmed_Musa_(cropped).jpg"
+    "profile_picture_uri": 'https://c.imge.to/2019/07/23/NJXbH.jpg'
    },
+]
+
+const testlist = [
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Andy Robertson',
+    avatar_url: 'https://c.imge.to/2019/07/23/NJXbH.jpg',
+    subtitle: 'Scotland Captain'
+  },
+  {
+    name: 'Andy Robertson',
+    avatar_url: 'https://c.imge.to/2019/07/23/NJXbH.jpg',
+    subtitle: 'Champions League Winner'
+  },
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  }
+  
 ]
 
 const Search = () => {
   return (
-    <View>
-    <SearchBar></SearchBar>
+    
+    <ScrollView>
+      {/* Attn CJ: Search query for name */}
+      <SearchBar></SearchBar>
       <Button
-        title="Filter (smaller button)" />
-      
-      {/* {
-        doctors.map((u) => {
-          return (
-            <DoctorInfoCard name={u.name} lang={u.languages}
-              profile_picture={u.profile_picture}></DoctorInfoCard>
-          )
-        })
-      } */}
-      
-    </View>
+        title="Filter" />
+        {
+          testlist.map((u) => (
+            <ListItem
+              leftAvatar={{ source:{uri: u.avatar_url} }}
+              //key={u.name}
+              title={u.name}
+              subtitle={u.subtitle}
+            />
+          ))
+        }
+      </ScrollView>
+
+  
   )
 }
 
 export default Search;
+
+// {
+//   doctors.map((u) => {
+//     return (
+//       <DoctorInfoCard name={u.name} lang={u.languages}
+//         profile_picture={u.profile_picture}></DoctorInfoCard>
+//     )
+//   })
+// } 
