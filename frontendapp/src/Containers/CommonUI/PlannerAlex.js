38bc194bbc1203eee59675be4e16e95a1cce9494
ncslzh <ncslzh@gmail.com>
2019-07-24 03:57:50 +0800

    Update PlannerAlex.js

--- frontendapp/src/Containers/CommonUI/PlannerAlex.js
+++ frontendapp/src/Containers/CommonUI/PlannerAlex.js
@@ -1,35 +1,97 @@
-import React, {Component} from 'React'
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
 import { Text } from 'react-native-elements'
-import { View } from 'react-native'
+import AppointmentSegment from '../../Components/Planner/AppointmentSegment';
 
 
-appointments_test = [
-    
+const jitsi_key = Math.random().toString(36).substring(2, 15) 
+                + Math.random().toString(36).substring(2, 15);
+
+
+const fakeData = [ //replaced with database fetch call in future
+    {
+        dateTime: "Friday 21 July 2019 6.00pm - 8.00pm",
+        person: "Dr. Steven Gerrard",
+        jitsi_key: Math.random().toString(36).substring(2, 15) 
+        + Math.random().toString(36).substring(2, 15),
+        apptType: "Pending"
+    },
+    {
+        dateTime: "Friday 21 July 2019 6.00pm - 8.00pm",
+        person: "Dr. Frank Lampard",
+        jitsi_key: Math.random().toString(36).substring(2, 15) 
+        + Math.random().toString(36).substring(2, 15),
+        apptType: "Upcoming"
+    },
+    {
+        dateTime: "Friday 21 July 2019 6.00pm - 8.00pm",
+        person: "Dr. Paul Scholes",
+        jitsi_key: Math.random().toString(36).substring(2, 15) 
+        + Math.random().toString(36).substring(2, 15),
+        apptType: "Upcoming"
+    },
 ]
 
-export default class PlannerAlex extends Component {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            appointments: appointments_test
+export default class Planner extends Component {
+    constructor() { //receive type and account completion as props
+        super();
+        this.state={
+            userData: [],
         }
     }
 
     componentDidMount() {
-        // for cj
+        //
     }
 
-    render(props) {
+
+    render() {
+        const { navigation } = this.props;
+        const type = navigation.dangerouslyGetParent().dangerouslyGetParent().getParam('type', 'nulltype');
+        const id = navigation.dangerouslyGetParent().dangerouslyGetParent().getParam('id', 'nullid')
+
         return (
-            <View>
-                <Text h4>Pending Appointments</Text>
+            <ScrollView>
+                <Text h4>Welcome back {id}</Text>
+                {/* {
+                    type === 'psychiatrist'
+                        ? <AppointmentSegment
+                            header="Pending Appointments"
+                            appointmentData={fakeData}
+                            type = {type}
+                            navigation = {navigation}
+                            />
+                        : null
+                } */}
 
-                <Text h4>Upcoming Appointments</Text>
+                <AppointmentSegment
+                    header="Pending Appointments"
+                    appointmentData={fakeData.filter(function(item) {
+                        return item.apptType == 'Pending';
+                    })}
+                    type = {type}
+                    navigation = {navigation}
+                    />
 
-                <Text h4>Past Appointments</Text>
-            </View>
-        )
+                <AppointmentSegment
+                    header="Upcoming Appointments"
+                    appointmentData={fakeData.filter(function(item) {
+                        return item.apptType == 'Upcoming';
+                    })}
+                    type = {type}
+                    navigation = {navigation}
+                    />
+
+                <AppointmentSegment
+                    header="Past Appointments"
+                    appointmentData={fakeData.filter(function(item) {
+                        return item.apptType == 'Past';
+                    })}
+                    type = {type}
+                    navigation = {navigation}
+                    />
+
+            </ScrollView>
+        );
     }
-
 }
