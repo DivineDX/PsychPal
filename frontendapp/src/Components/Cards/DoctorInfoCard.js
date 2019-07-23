39d77019e224e6dc1b915d167fbfe6b3f783c36b
alxkohh <49210156+alxkohh@users.noreply.github.com>
2019-07-23 21:03:58 +0800

    resolve

--- frontendapp/src/Components/Cards/DoctorInfoCard.js
+++ frontendapp/src/Components/Cards/DoctorInfoCard.js
@@ -3,18 +3,7 @@ import React from 'react'
 
 const DoctorCard = (props) => {
     return (
-        <Card
-  title='HELLO WORLD'
-  image={require('../images/pic2.jpg')}>
-  <Text style={{marginBottom: 10}}>
-    The idea with React Native Elements is more about component structure than actual design.
-  </Text>
-  <Button
-    icon={{name: 'code'}}
-    backgroundColor='#03A9F4'
-    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-    title='VIEW NOW' />
-</Card>
+       null
         // <Card title={props.name}>
         //     <Avatar
         //         size='large'
