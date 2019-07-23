f9833eeec02a4c9c49d7bda6a82daf0b3680d406
ncslzh <ncslzh@gmail.com>
2019-07-24 03:12:29 +0800

    Update AddTreatment.js

--- frontendapp/src/Containers/PsychUI/AddTreatment.js
+++ frontendapp/src/Containers/PsychUI/AddTreatment.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native'
 import { Input, Text } from 'react-native-elements';
 import { Textarea } from "native-base";
 
-const AddTreatment = () => {
-  return (
-    <View>
-      <Text h4>{`\n  `}Create treatment plan{'\n  '}</Text>
 
-      <Input
-        placeholder='Title'
-      />
+export default class AddTreatment extends Component {
+    constructor() {
+        super()
+        this.state={
+            userData:[],
+            name: 'Ashley',
+        }
+    }
 
-      <Textarea style = {styles.textboxContainer} rowSpan={5} bordered placeholder="Instructions" />
+    componentDidMount() {
+        // for cj
+    }
 
-    </View>
-  );
-};
+    render() {
+        return (
+            <View>
+                <Text h4>{`\n  `}Add treatment for {this.state.name + '\n'}</Text>
+
+                <Input
+                    placeholder='Title'
+                />
+
+                <Textarea style = {styles.textboxContainer} rowSpan={5} bordered placeholder="Instructions" />
+
+            </View>
+        )
+    }
+}
 
 const styles = StyleSheet.create({
   textboxContainer: {
     marginHorizontal: 20,
   }
 })
-export default AddTreatment;
