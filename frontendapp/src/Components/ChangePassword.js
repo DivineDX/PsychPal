3908d196710953a9ef4f7e43fdf5c441815f0972
ncslzh <ncslzh@gmail.com>
2019-07-26 13:50:43 +0800

    change password form completed

--- frontendapp/src/Components/ChangePassword.js
+++ frontendapp/src/Components/ChangePassword.js
@@ -18,7 +18,7 @@ export default class ChangePassword extends Component {
             user : test_user,
             input_current: 'current',
             input_new: 'new',
-            input_re_enter: 're-enter'
+            input_new_confirm: 'new_confirm'
         }
     }
 
@@ -34,38 +34,42 @@ export default class ChangePassword extends Component {
         })
     }
 
-    update_input_re_enter = (text) => {
+    update_input_new_confirm = (text) => {
         this.setState({
-            input_re_enter: text
+            input_new_confirm: text
         })
     }
 
     handleInputs = () => {
         if (this.state.input_current != this.state.user.password) {
-            
+            alert("Incorrect Current Password. Please re-enter.")
+        } else {
+            if (this.state.input_new == this.state.input_new_confirm) {
+                // Attn CJ: CHANGE USER PASSWORD to this.state.input_new
+                alert("Password succesfully changed.")
+            } else {
+                alert("New Password and Confirm New Password do not match. Please re-enter.")
+            }
         }
     }
 
     componentDidMount() {
-        // for cj
+        // for CJ
     }
 
     render() {
-        alert("hi world")
         return (
             <View>
-                {/* Attn CJ: Password they declare here MUST == password in that user object */}
-                <Input placeholder='Your current password' onChangeText={this.update_input_current} />
-                {/* Attn CJ: New password they declare here MUST != password in that user object */}
-                <Input placeholder='New password' onChangeText={this.update_input_new} />
-                <Input placeholder='Re-enter new password' onChangeText={this.update_input_re_enter }/>
+                <Input placeholder='Current Password' onChangeText={this.update_input_current} />
+                <Input placeholder='New Password' onChangeText={this.update_input_new} />
+                <Input placeholder='Confirm New Password' onChangeText={this.update_input_new_confirm }/>
                 <Button 
                     title="Confirm"
                     onPress={this.handleInputs} />
                 <Text h4>All three inputs shown below for sanity check</Text>
                 <Text h4>{this.state.input_current}</Text>
                 <Text h4>{this.state.input_new}</Text>
-                <Text h4>{this.state.input_re_enter}</Text>
+                <Text h4>{this.state.input_new_confirm}</Text>
             </View>
         )
     }
