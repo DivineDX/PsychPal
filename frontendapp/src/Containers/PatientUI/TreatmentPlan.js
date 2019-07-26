4d82515f0dbd6d655e8e0aa21af3765ab8e6438b
ncslzh <ncslzh@gmail.com>
2019-07-26 10:14:53 +0800

    Refactored treatment and its child components

--- frontendapp/src/Containers/PatientUI/TreatmentPlan.js
+++ frontendapp/src/Containers/PatientUI/TreatmentPlan.js
@@ -1,86 +1,102 @@
-import React, { Component } from 'react'
-import { Text } from 'react-native-elements'
-import { ScrollView } from 'react-native'
-import AddTreatmentButton from '../../Components/Buttons/AddTreatmentButton'
-import TreatmentPlanCard from '../../Components/Cards/TreatmentPlanCard'
-
+import React, { Component } from "react";
+import { ScrollView } from "react-native";
+import { Text, Button } from "react-native-elements";
+import TreatmentPlanCard from "../../Components/Cards/TreatmentPlanCard";
 
 // Attn CJ: Data required here is an array of treatment objects for that 1 particular patient
 const treatment_plan_test = [
-    {   
-        "user_type": "Patient",
-        "patient_name": "Christian Eriksen",
-        "treatment_title": "Strepsils",
-        "treatment_details": "Placebo for PTSD. 2 times a day. Complete course."
-    },
-    {   
-        "user_type": "Patient",
-        "patient_name": "Christian Eriksen",
-        "treatment_title": "Panadol",
-        "treatment_details": "Panadol cures anything. 3 times a day."
-    },
-    {   
-        "user_type": "Patient",
-        "patient_name": "Christian Eriksen",
-        "treatment_title": "Panadol",
-        "treatment_details": "Panadol cures anything. 3 times a day."
-    },
-    {   
-        "user_type": "Patient",
-        "patient_name": "Christian Eriksen",
-        "treatment_title": "Panadol",
-        "treatment_details": "Panadol cures anything. 3 times a day."
-    },
-    {   
-        "user_type": "Patient",
-        "patient_name": "Christian Eriksen",
-        "treatment_title": "Panadol",
-        "treatment_details": "Panadol cures anything. 3 times a day."
-    },
-    {   
-        "user_type": "Patient",
-        "patient_name": "Christian Eriksen",
-        "treatment_title": "Panadol",
-        "treatment_details": "Panadol cures anything. 3 times a day."
-    },
-    {   
-        "user_type": "Patient",
-        "patient_name": "Christian Eriksen",
-        "treatment_title": "KitKat",
-        "treatment_details": "Have a break. Have a KitKat."
-    }
-]
+	{
+		user_type: "Patient",
+		patient_name: "Christian Eriksen",
+		treatment_title: "Strepsils",
+		treatment_details: "Placebo for PTSD. 2 times a day. Complete course."
+	},
+	{
+		user_type: "Patient",
+		patient_name: "Christian Eriksen",
+		treatment_title: "Panadol",
+		treatment_details: "Panadol cures anything. 3 times a day."
+	},
+	{
+		user_type: "Patient",
+		patient_name: "Christian Eriksen",
+		treatment_title: "Panadol",
+		treatment_details: "Panadol cures anything. 3 times a day."
+	},
+	{
+		user_type: "Patient",
+		patient_name: "Christian Eriksen",
+		treatment_title: "Panadol",
+		treatment_details: "Panadol cures anything. 3 times a day."
+	},
+	{
+		user_type: "Patient",
+		patient_name: "Christian Eriksen",
+		treatment_title: "Panadol",
+		treatment_details: "Panadol cures anything. 3 times a day."
+	},
+	{
+		user_type: "Patient",
+		patient_name: "Christian Eriksen",
+		treatment_title: "Panadol",
+		treatment_details: "Panadol cures anything. 3 times a day."
+	},
+	{
+		user_type: "Patient",
+		patient_name: "Christian Eriksen",
+		treatment_title: "KitKat",
+		treatment_details: "Have a break. Have a KitKat."
+	}
+];
 
 export default class TreatmentPlan extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            treatment_plan: treatment_plan_test
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			treatment_plan: treatment_plan_test
+		};
+	}
 
-    componentDidMount() {
-        // for cj
-    }
+	componentDidMount() {
+		// for cj
+	}
 
-    render() {
-        return (
-            <ScrollView>
-                 <Text h4>Treatment Plan for {this.state.treatment_plan[0].patient_name}</Text>
-                <AddTreatmentButton user_type={this.state.treatment_plan[0].user_type}></AddTreatmentButton>
-                {
-                    this.state.treatment_plan.map((u) => {
-                        return (
-                            <TreatmentPlanCard
-                                treatment_title={u.treatment_title}
-                                treatment_details={u.treatment_details}>
-                            </TreatmentPlanCard>
-                        )
-                    })
-                }
-            </ScrollView>
-        )
-    }
+	render() {
+		if (this.state.treatment_plan[0].user_type == "Patient") {
+			return (
+				<ScrollView>
+					<Text h4>
+						Treatment Plan for {this.state.treatment_plan[0].patient_name}
+					</Text>
+					{this.state.treatment_plan.map(u => {
+						return (
+							<TreatmentPlanCard
+								user_type={u.user_type}
+								treatment_title={u.treatment_title}
+								treatment_details={u.treatment_details}
+							/>
+						);
+					})}
+				</ScrollView>
+			)
+		} else {
+			return (
+				<ScrollView>
+					<Text h4>
+						Treatment Plan for {this.state.treatment_plan[0].patient_name}
+					</Text>
+					<Button title="Add Treatment" />
+					{this.state.treatment_plan.map(u => {
+						return (
+							<TreatmentPlanCard
+								user_type={u.user_type}
+								treatment_title={u.treatment_title}
+								treatment_details={u.treatment_details}
+							/>
+						);
+					})}
+				</ScrollView>
+			);
+		}
+	}
 }
-
-      
